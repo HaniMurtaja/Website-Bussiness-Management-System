89bcc3811c0640a86036a9a5f75cec4bd4e09420
Hani Murtaja <hanimurtaja@Hanis-MacBook-Pro.local>
2023-01-16 15:48:58 +0200

    migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,12 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
-        Schema::defaultStringLength(191);
-        $home_page_variant = get_static_option('home_page_variant');
-        view()->share(compact('home_page_variant'));
-        if (get_static_option('site_force_ssl_redirection') === 'on'){
-            URL::forceScheme('https');
-        }
+        
     }
 }
